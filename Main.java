53e1c0e9080d3b07783230797a6d1ab2cab2e836
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-10 00:00:36 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,31 +1,27 @@
 public class Main {
 
-    // Helper method to check if a number is prime
-    public static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false; // 0 and 1 are not prime numbers
+    public static String convertToBinary(int decimalNum) {
+        if (decimalNum == 0) {
+            return "0";
         }
-        // Check for factors from 2 up to the square root of num
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                return false; // Found a factor, so it's not prime
-            }
+
+        String binaryResult = "";
+
+        while (decimalNum > 0) {
+            int remainder = decimalNum % 2;
+            binaryResult = remainder + binaryResult;
+            decimalNum = decimalNum / 2;
         }
-        return true; // No factors found, it is prime
+
+        return binaryResult;
     }
 
-    // Method from your image to print all primes in the range [2, n]
-    public static void primesInRange(int n) {
-        for (int i = 2; i <= n; i++) {
-            if (isPrime(i)) { // if true
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
-    }
-
-    // Main method to run the program
-    public static void main(String args[]) {
-        primesInRange(20); // Prints primes from 2 to 20
+    public static void main(String[] args) {
+        int numberToConvert = 13;
+        
+        String binaryString = convertToBinary(numberToConvert);
+        
+        System.out.println("Decimal: " + numberToConvert);
+        System.out.println("Binary:  " + binaryString);
     }
 }
