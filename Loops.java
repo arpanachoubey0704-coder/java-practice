c2c309a29e834c8deba097befce4723a3dd3ff39
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-18 20:25:51 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Loops{
 public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  System.out.print(" Enter no.here : ");
+  int n = sc.nextInt();
   int num = 1;
-  while (num<=100){
+  while (num<=n){
     System.out.println(num);
     num++;
   }
   System.out.println("congrts your number are here");
+  sc.close();
 }
 }
