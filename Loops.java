35a3c145734e59502c27657ee5fb750efd495aec
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-18 20:40:51 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class Loops{
-public static void main(String[] args) {
-  Scanner sc = new Scanner(System.in);
-  System.out.print(" Enter no.here : ");
-  int n = sc.nextInt();
-  int num = 1;
-  while (num<=n){
-    System.out.println(num);
-    num++;
+public class Loops {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print( " Enter first num : ");
+    
+    int n= sc.nextInt();
+
+    int sum = 0;
+    int i = 1;
+
+    while (i<=n) {
+      sum = sum +i;
+      i++;
+    }
+    System.out.println(+n +sum);
   }
-  System.out.println("congrts your number are here");
-  sc.close();
-}
 }
