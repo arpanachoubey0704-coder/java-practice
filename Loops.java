921f8571ad8b10ba975890649215fba57e5c1075
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-18 20:16:40 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,16 +1,9 @@
-import java.util.Scanner;
-
-public class Loops {
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter num here : ");
-    int num = sc.nextInt();
-while (num <=100){
-  System.out.println(+num);
-  num++;
-
-}
-sc.close();
+public class Loops{
+public static void main(String[] args) {
+  int num = 1;
+  while (num<=10){
+    System.out.println(num);
+    num++;
   }
+}
 }
