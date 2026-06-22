77370b5d8f621d9abec8c97f57994b842c182b83
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-22 08:13:41 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,8 +1,12 @@
 public class function {
   public static void main (String []args){
-    int a = 6;
-    int b = 8;
-    int sum = a + b ;
-    System.out.println(sum);
+    int a = 2;
+    int b = 5;
+    System.out.println("Before swapping a = " +a + " , b=" +b);
+    int swap = a ;
+    a = b ;
+    b = swap;
+    System.out.println("After Swapping a = " +a+ " , n=" +b );
   }
+
 }
