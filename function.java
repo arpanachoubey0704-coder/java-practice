e619bc1cd0363a7592e59ec5fde5c251983439aa
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-23 22:05:40 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,12 +1,25 @@
-public class function {
-  public static void main (String []args){
-    int b = 2;
-    int c = 5;
-    System.out.println("Before swapping a = " +b + " , b=" +b);
-    int swap = b ;
-    b = c ;
-    c = swap;
-    System.out.println("After Swapping a = "+ b+ " , n=" +b );
-  }
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
+public class function {
+    public static void main(String[] args) {
+        int n = 100000;
+
+        List<Integer> arrayList = new ArrayList<>();
+        long start = System.nanoTime();
+        for (int i = 0; i < n; i++) {
+            arrayList.add(0, i);
+        }
+        long end = System.nanoTime();
+        System.out.println("ArrayList insertion at beginning: " + (end - start) / 1000000.0 + " ms");
+
+        List<Integer> linkedList = new LinkedList<>();
+        start = System.nanoTime();
+        for (int i = 0; i < n; i++) {
+            linkedList.add(0, i);
+        }
+        end = System.nanoTime();
+        System.out.println("LinkedList insertion at beginning: " + (end - start) / 1000000.0 + " ms");
+    }
 }
