12c43dcc41021c2adc309dbaa27333deac179e35
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-24 21:26:26 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,14 +1,1 @@
-public class Main {
 
-    public static void main(String[] args) {
-        int n = 3;
-        char ch = 'A';
-        for (int line = 1 ; line<=n; line ++){
-            for ( int chars = 1 ; chars <=(n-line+1) ; chars ++){
-System.out.print( ch );
-ch++;
-            }
-            System.out.println();
-        }
-    }
-}
