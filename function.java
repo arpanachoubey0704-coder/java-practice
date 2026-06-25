1582ca99b8dd8377103b838f5daad327e3997104
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-25 18:53:59 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,12 +1,14 @@
 //import java.util.*;
 public class function {
-  public static void name (){ 
-    int a = 5;
-    int b = 3 ;
+  public static void name (int a , int b ){ 
+   
     int sum = a + b ;  
     System.out.println(" sum is : " +sum);
   }
   public static void main(String[] args) {
-    name();
+ int a = 5;
+    int b = 3 ;
+    name(5,3);
+    
   }
 }
