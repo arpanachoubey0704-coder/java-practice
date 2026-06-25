5743c9585060963c8fb4bb6f57852d3f9cdc68b5
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-25 18:49:55 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,30 +1,9 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
+import java.util.*;
 public class function {
-    public static void main(String[] args) {
-        int n = 50000;
-        List<Integer> arrayList = new ArrayList<>();
-        List<Integer> linkedList = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            arrayList.add(i);
-            linkedList.add(i);
-        }
-
-        long start = System.nanoTime();
-        for (int i = 0; i < n; i++) {
-            arrayList.get(i);
-        }
-        long end = System.nanoTime();
-        System.out.println("ArrayList access time: " + (end - start) / 1000000 + " ms");
-
-        start = System.nanoTime();
-        for (int i = 0; i < n; i++) {
-            linkedList.get(i);
-        }
-        end = System.nanoTime();
-        System.out.println("LinkedList access time: " + (end - start) / 1000000 + " ms");
-    }
+  public static void name (){
+    System.out.println("Hellow arpana");
+  }
+  public static void main(String[] args) {
+    name();
+  }
 }
