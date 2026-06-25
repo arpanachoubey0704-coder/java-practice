5780a5cf7d2d0833d74023b64cadc60d69b84009
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-25 18:52:00 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,7 +1,10 @@
-import java.util.*;
+//import java.util.*;
 public class function {
-  public static void name (){
-    System.out.println("Hellow arpana");
+  public static void name (){ 
+    int a = 5;
+    int b = 3 ;
+    int sum = a + b ;  
+    System.out.println(" sum is : " +sum);
   }
   public static void main(String[] args) {
     name();
