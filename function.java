bfd51b31b019561762d5f8ac931096931a325d3a
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-27 06:41:16 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
 
 public class function {
-  public static void num (){
+  public static void num (int a , int b ){
 //Scanner sc = new Scanner(System.in);
-Scanner sc = new Scanner(System.in);
-System.out.print(" Enter num a here : ");
+// Scanner sc = new Scanner(System.in);
+// System.out.print(" Enter num a here : ");
+
+// int a = sc.nextInt();
+// System.out.print( " Enter num b here : ");
+// int b = sc.nextInt();
+int sum = a + b ;
+System.out.print(" The total sum is : " + sum );
+//sc.close();
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print(" Enter num a here : ");
 
 int a = sc.nextInt();
 System.out.print( " Enter num b here : ");
 int b = sc.nextInt();
-int sum = a + b ;
-System.out.print(" The total sum is : " + sum );
-sc.close();
-  }
-  public static void main(String[] args) {
-    num ();
+    num (a,b);
     //System.out.println(+sum);
+    sc.close();
   }
   //System.out.println(+sum);
 
