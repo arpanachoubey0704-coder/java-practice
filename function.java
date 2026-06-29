e34a8859b11afe9dc454ea42298914479c3af3ad
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-06-29 22:22:26 +0530

    Initial commit

--- function.java
+++ function.java
@@ -5,7 +5,7 @@ public class function {
     }
 
     public static void main(String[] args) {
-        int myNumber = 8;
+        int myNumber = 9;
         
         int result = calculateSquare(myNumber);
         
