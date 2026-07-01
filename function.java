7b47bfdf55c17e3d8192a8b100ef5975cfb2bcb9
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-01 20:46:10 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,14 +1,6 @@
 public class function {
-
-    public static int calculateSquare(int number) {
-        return number * number;
-    }
-
     public static void main(String[] args) {
-        int myNumber = 20;
-        
-        int result = calculateSquare(myNumber);
-        
-        System.out.println("The square of " + myNumber + " is: " + result);
+        int a = 9 ;
+        System.out.print(a);
     }
 }
