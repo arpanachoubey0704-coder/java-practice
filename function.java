9f7fbe6cbbcec9c12c53028b48c4e15b80ab84f4
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-01 20:48:04 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,7 +1,7 @@
 public class function {
     public static void main(String[] args) {
         int a = 9 ;
-        int b = a;
+        int b = 90;
         int sum = a + b ;
         System.out.print(sum);
     }
