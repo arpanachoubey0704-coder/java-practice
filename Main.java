d7bbe840615601e96cbabe33cf54ea21ecac1966
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-02 18:50:20 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ public class Main {
     Scanner sc = new Scanner ((System.in));
     System.out.print("Enter num a : ");
     int a = sc.nextInt();
-    System.out.print(" number a is : " +a);
+     System.out.print("Enter num b : ");
+    int b = sc.nextInt();
+     System.out.print("Enter num c : ");
+    int c = sc.nextInt();
+    System.out.println(" number a is : " +a);
+       System.out.println(" number a is : " +b);
+          System.out.println(" number a is : " +c);
     sc.close();
   }
 }
