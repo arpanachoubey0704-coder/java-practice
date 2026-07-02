eac6306a9b3f533ef4eb99ea64f28af21ada78d8
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-02 18:44:43 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
 
+public class Main {
+  public static void main (String[]args){
+    Scanner sc = new Scanner ((System.in));
+    System.out.print("Enter num a : ");
+    int a = sc.nextInt();
+    System.out.print(" number a is : " +a);
+    sc.close();
+  }
+}
--- function.java
+++
@@ -1,8 +0,0 @@
-public class function {
-    public static void main(String[] args) {
-        int a = 9 ;
-        int b = 90;
-        int sum = a + b ;
-        System.out.print(sum);
-    }
-}
