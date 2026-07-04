50ee5d0fed4591e2be09ec59fe42fcd8760d89b1
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-04 13:11:55 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
 
 public class Loops {
+
   public static void main(String[] args) {
-    System.out.print( " Enter the Latter : ");
     Scanner sc = new Scanner(System.in);
-    char ch = sc.next().charAt(0);
-    int ASCII = (int) ch;
-    System.out.print(" The ASCII value of  " +ch + "  is : " + ASCII);
-    sc.close();
+    System.out.print(" Enter num A here : ");
+    int a = sc.nextInt(); 
+
+    System.out.print(" Enter num B here : ");
+    int b = sc.nextInt();
+     
+    if(a>b){
+      System.out.print(+a + "is greater then B");
+    }
+    else{
+      System.out.print( +a+ " is less then B");
+      sc.close();
+    }
   }
 }
