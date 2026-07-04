606cdfaf75fe18a89b25565df05adb8b0342c297
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-04 11:06:01 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,13 +1,16 @@
 public class Loops {
   public static void main(String[] args) {
-    int a = 8;
-    int b = 9;
-    //int temp = a ;
-    a = a ^ b ;
-    b  = a ^ b ;
-    a = a ^ b ;
+    // int a = 8;
+    // int b = 9;
+    // //int temp = a ;
+    // a = a ^ b ;
+    // b  = a ^ b ;
+    // a = a ^ b ;
+    // System.out.println(" a : " +a);
+    // System.out.println(" b : " +b);
   
-    System.out.println(" a : " +a);
-    System.out.println(" b : " +b);
+  float r  = 2;
+   float a = 3.14f * r * r ;
+System.out.println(" Area of sqare : " +a);
   }
 }
