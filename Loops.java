77b231ecd72955ce09baafe460602c730c455879
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-04 11:29:23 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,16 +1,12 @@
+import java.util.Scanner;
+
 public class Loops {
   public static void main(String[] args) {
-    // int a = 8;
-    // int b = 9;
-    // //int temp = a ;
-    // a = a ^ b ;
-    // b  = a ^ b ;
-    // a = a ^ b ;
-    // System.out.println(" a : " +a);
-    // System.out.println(" b : " +b);
-  
-  float r  = 2;
-   float a = 3.14f * r * r ;
-System.out.println(" Area of sqare : " +a);
+    System.out.print( " Enter the Latter : ");
+    Scanner sc = new Scanner(System.in);
+    char ch = sc.next().charAt(0);
+    int ASCII = (int) ch;
+    System.out.print(" The ASCII value of  " +ch + "  is : " + ASCII);
+    sc.close();
   }
 }
