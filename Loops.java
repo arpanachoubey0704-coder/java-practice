f920b87d47e74df69b69d361b36215c9b25816b4
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-04 10:53:16 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -3,9 +3,9 @@ public class Loops {
     int a = 8;
     int b = 9;
     //int temp = a ;
-    a = a+b ;
-    b  = a - b ;
-    a = a - b ;
+    a = a ^ b ;
+    b  = a ^ b ;
+    a = a ^ b ;
   
     System.out.println(" a : " +a);
     System.out.println(" b : " +b);
