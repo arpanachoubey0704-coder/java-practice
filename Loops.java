2d3a7d1d3b701d7ed5aee120081bf52e35123d24
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-05 16:36:02 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,21 +1,9 @@
-import java.util.Scanner;
-
-public class Loops {
-
+public class Loops{
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter num A here : ");
-    int a = sc.nextInt(); 
-
-    System.out.print(" Enter num B here : ");
-    int b = sc.nextInt();
-     
-    if(a>b){
-      System.out.print(+a + "is greater then B");
-    }
-    else{
-      System.out.print( +a+ " is less then B");
-      sc.close();
+    int counter = 1;
+    while(counter<=100){
+      System.out.println(counter);
+      counter++;
     }
   }
 }
