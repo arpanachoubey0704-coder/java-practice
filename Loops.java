8939e9675b64d97ccb8b92cea9d7634b41766138
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-05 16:42:14 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Loops{
   public static void main(String[] args) {
+    System.out.print("enter num here : ");
+    Scanner sc = new Scanner(System.in);
+    int range = sc.nextInt();
     int counter = 1;
-    while(counter<=100){
-      System.out.println("hellow world");
+    while(counter<=range){
+      System.out.print(counter  +" ");
       counter++;
+      sc.close();
     }
   }
 }
