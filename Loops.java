357748838fcbe080301ff899d6e9dab998446202
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-05 16:36:31 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -2,7 +2,7 @@ public class Loops{
   public static void main(String[] args) {
     int counter = 1;
     while(counter<=100){
-      System.out.println(counter);
+      System.out.println("hellow world");
       counter++;
     }
   }
