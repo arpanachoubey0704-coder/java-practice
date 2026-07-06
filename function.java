aea8e3457596a62582e8787fc788f955ba3cdecb
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-06 07:12:12 +0530

    Initial commit

--- function.java
+++ function.java
@@ -3,8 +3,9 @@ public static void num() {
 
   int a  = 6;
     int b = 5;
-    System.out.println(+a);
-     System.out.println(+b);
+    int sum = a + b;
+    System.out.println(+sum);
+     //System.out.println(+b);
 }
   public static void main(String[] args) {
     num();
