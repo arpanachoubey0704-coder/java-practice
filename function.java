9150d907ca3f5d18f9e17ed36b6cac3fb8fa2d54
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-06 07:04:49 +0530

    Initial commit

---
+++ function.java
@@ -0,0 +1,13 @@
+public class function {
+
+  public static void hellow(){
+    System.out.println(" Hellow World ");
+    System.out.println(" Hellow World ");
+    System.out.println(" Hellow World ");
+System.out.print(" Hellow World ");
+return;
+  }
+  public static void main(String[] args) {
+    hellow();
+  }
+}
