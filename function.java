4c67ba3c5a49e4e5b59320e1beefa55ac365b6b6
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-08 22:18:06 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,11 +1,3 @@
 public class function {
-public static void num (){
-  int n = 8;
-  int m = 9;
-  System.out.println(+n +m);
-  return;
-}
-  public static void main(String[] args) {
-    num();
-  }
+  
 }
