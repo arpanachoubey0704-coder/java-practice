f3fbe4f3de6699bc55f8d53cf2fad0db97a72be1
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-09 22:49:12 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class function {
 
   public static int calculatesum () {
-    int a = 78;
-    int b = 90;
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
 int sum = a + b ;
+sc.close();
 return sum ;
+// sc.close();
   }
   public static void main(String[] args) {
     int totalsum = calculatesum();
     System.out.println(totalsum);
+    
   }
 }
