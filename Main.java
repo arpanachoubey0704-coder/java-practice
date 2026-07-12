21b14c4b0c335282061c3d8ff88f782a643eb34e
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-12 11:06:34 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Main{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    if (a<=10){
+    //int a = sc.nextInt();
+    for (int a = 1; a <=10; a++){
 System.out.println(a);
+sc.close();
     }
   
   }
