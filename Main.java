21285f85f681cd8da303b77141f86a9213f3f94a
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-12 11:05:29 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Main{
   public static void main(String[] args) {
-    int a = 1;
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
     if (a<=10){
 System.out.println(a);
     }
+  
   }
 }
