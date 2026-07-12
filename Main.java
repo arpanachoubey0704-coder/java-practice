40665aee4500f14e0206472bdcf8fe05b35255aa
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-12 10:54:06 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,23 +1,8 @@
-import java.util.Scanner;
-
-public class Main {
+public class Main{
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter the num : ");
-    int n = sc.nextInt();
-    if ( n%2 !=00 ){
-      System.out.println("weird");
-    }else{
-      if (n>=2&&n<=5){
-    System.out.println("not werid");
+    int a = 1;
+    if (a<=10){
+System.out.println(a);
     }
-    else if (n>=6&&n<=20){
-System.out.println("werid");
-    }
-    else{
-      System.out.println("not weird");
-    }
-  }
-    sc.close();
   }
 }
