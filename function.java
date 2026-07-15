9fea3a95ff68ffb14d7ff7aeaca0ad301a4acd45
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-15 22:57:16 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,8 +1,8 @@
 public class function {
 
   public static void main(String[] args) {
-    for (int i =1; i<=4; i++){
-System.out.println("*");
+    for (int i=4; i<=4; i++){
+System.out.println("****");
     }
   }
 }
