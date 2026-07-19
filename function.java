f4c250b5ab5bb7de125625eb48b435dd2536e726
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-19 21:56:15 +0530

    Initial commit

--- function.java
+++ function.java
@@ -4,7 +4,7 @@ class function{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        switch(1) {
+        switch(n) {
             case 1 : System.out.println (" One ");
             break;
              case 2 : System.out.println (" Two ");
