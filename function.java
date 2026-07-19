5b97d01681ea0145735a797c032d6412732512ac
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-19 21:55:10 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,16 +1,30 @@
-public class function {
-    public static boolean isPalindrome(int number) {
-        if (number < 0) return false;
-        int original = number;
-        int reversed = 0;
-        for (; number > 0; number /= 10) {
-            int digit = number % 10;
-            reversed = reversed * 10 + digit;
-        }
-        return original == reversed;
-    }
+import java.util.Scanner;
+
+class function{
     public static void main(String[] args) {
-        int num = 132321;
-        System.out.println(num + (isPalindrome(num) ? " is a palindrome." : " is not a palindrome."));
-    }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        switch(n) {
+            case 1 : System.out.println (" One ");
+            break;
+             case 2 : System.out.println (" Two ");
+              break;
+              case 3 : System.out.println (" Three ");
+               break;
+               case 4 : System.out.println (" Four ");
+                break;
+                case 5 : System.out.println (" Five ");
+                 break;
+                 case 6 : System.out.println (" Six ");
+                  break;
+                  case 7 : System.out.println (" Seven ");
+                   break;
+                   case 8 : System.out.println (" Eight ");
+                    break;
+                    case 9 : System.out.println (" Nine ");
+                    break;
+                    default : System.out.println("Unknown");
+}
+sc.close();
+}
 }
