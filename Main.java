8d3f8b6c803c0e4a647b5aeadc54dab7cf44e470
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-23 21:40:01 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void greet (){
@@ -5,10 +7,16 @@ System.out.println("hellow welcome to java");
 return;
     }
     public static void addnumber(int num1 , int num2){
+        
         int sum= num1+num2;
 System.out.print(" Sum is : " +sum);
     }
     public static void main(String[] args) {
-        addnumber(2, 3);
+        //addnumber(num1, num2);
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b = sc.nextInt();
+        addnumber(a, b);
+        sc.close();
     }
 }
