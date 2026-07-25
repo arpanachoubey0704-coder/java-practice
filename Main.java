4ee72409b82bc49b5a8d20ae464585bca9c04a2d
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-25 21:49:08 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
-
 public class Main{
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
-if(n<=10){
-    System.out.println(n);
-}
-else {
-    System.out.println("number is not prime");
-}
-}
+    public static void main(String[] args) {
+        System.out.println("hii");
+    }
 }
