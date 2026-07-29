a7fd0802530ffdfcfdbf3166454858806126a8a3
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-07-29 10:10:35 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
-public class Main{
-    public static void code() {
-        int n = 9;
-           int m = 11;
-              int s= n+m;
-        System.out.println(s);
-    }
-    public static void main(String[] args) {
-       code(); 
-    }
+public class Main {
+
+    
+
+public static void main(String[] args) {
+    System.out.println("hii");
+}
 }
