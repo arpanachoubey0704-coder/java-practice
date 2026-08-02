079a23eb48c720b02d862b7b5b036d14ac5921cf
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-02 21:23:07 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        int a = 7;
-        int b=0;
-        int product = a*b;
-        System.out.println(product);
+        System.out.println("Hellow World");
     }
 }
