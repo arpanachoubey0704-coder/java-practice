4bbbea2ed4d14c0b5129ba6402d0977c4f9c388d
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-03 10:39:27 +0530

    Initial commit

--- variableanddatatype.java
+++ variableanddatatype.java
@@ -1,13 +1,20 @@
 public class variableanddatatype {
 
   public static void main(String[] args) {
-    int a = 11;
-    int b = 2;
-    int sum = a+b;
-  int sub = a-b;
-    int pro = a*b;
-      double div = a/b;
-      int modules = a%b;
-      System.out.println(div);
+  //   int a = 11;
+  //   int b = 2;
+  //   int sum = a+b;
+  // int sub = a-b;
+  //   int pro = a*b;
+  //     double div = a/b;
+  //     int modules = a%b;
+  //     System.out.println(div);
+  int a  = 20;
+if(a>10){
+  System.out.println("greater");
+}
+else{
+  System.out.println("less");
+}
   }
 }
