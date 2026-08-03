ff6aba537d7b8d8643cf098b18da98969e9ca325
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-03 10:08:47 +0530

    Initial commit

--- variableanddatatype.java
+++ variableanddatatype.java
@@ -1,21 +1,9 @@
-import java.util.Scanner;
 public class variableanddatatype {
-public static void main(String[] args) {
-  Scanner sc = new Scanner(System.in);
-  System.out.print(" Eneter pencil value : ");
-  float pencil = sc.nextFloat();
-  System.out.print(" Eneter pen value : ");
-  float pen = sc.nextFloat();
-  System.out.print(" Eneter eraser value : ");
-  float eraser = sc.nextFloat();
 
-  float total = (pencil+pen+eraser);
-  System.out.println("total you buy : " +total);
-  float gst = total * 0.18f;
-  float finalbill = total + gst;
-  System.out.println( " Gst is : " +gst);
-  
-  System.out.print("final bil : " +finalbill);
-  sc.close();
-}
+  public static void main(String[] args) {
+    //long _result= 9.8l;
+    int a = 9;
+    byte b = (byte)a;
+    System.out.println(b);
+  }
 }
