31fdde680cd6432414a3009be4aeb8187c002c70
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-04 16:02:57 +0530

    Initial commit

--- variableanddatatype.java
+++ variableanddatatype.java
@@ -1,17 +1,15 @@
-import java.util.Scanner;
+//Palindrome number 
+public class variableanddatatype {
 
-public class variableanddatatype{
- public static void main(String[]args){
-  Scanner sc = new Scanner(System.in);
-  System.out.print(" Enter number here : ");
-  int a = sc.nextInt();
-  
-  if(a%2==0){
-System.out.println("No. is even");
+  public static void main(String[] args) {
+    int num = 12787891;
+    int firstdigit = num/100;
+    int lastdigit = num%10;
+    if(firstdigit==lastdigit){
+      System.out.println("num is palindrome");
+    }
+    else{
+      System.out.println("num is not plaindrome");
+    }
   }
-  else{
-    System.out.println(" No. is odd");
-    sc.close();
-  }
- }
 }
