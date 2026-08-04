9c9261966a0c310869a7e1b0df606b080e953089
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-04 15:56:20 +0530

    Initial commit

--- variableanddatatype.java
+++ variableanddatatype.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class variableanddatatype{
  public static void main(String[]args){
-  int a = 9;
+  Scanner sc = new Scanner(System.in);
+  System.out.print(" Enter number here : ");
+  int a = sc.nextInt();
+  
   if(a%2==0){
-System.out.println("even");
+System.out.println("No. is even");
   }
   else{
-    System.out.println("odd");
+    System.out.println(" No. is odd");
+    sc.close();
   }
  }
 }
