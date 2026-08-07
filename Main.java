73f181cd9ab460f6426b97f2ac2e5f9867acfe73
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-07 19:13:39 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main{
-    public static void main(String[] args) {
-        int  a = 10;
-        System.out.println("Hellow World");
+    public void result(){
+        int  a = 80;
+        System.out.print(a);
     }
+    public static void main(String[] args) {
+        Main w = new Main();
+    w.result();
+    }
+
 }
