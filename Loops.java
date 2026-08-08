5b7a6c7d69a01add9d277d412d09153775587215
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-08 19:29:05 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,24 +1,15 @@
-import java.util.Scanner;
-
 public class Loops {
-
   public static void main(String[] args) {
-   Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a number here : ");
-int a = sc.nextInt();
- System.out.print("Enter b number here : ");
-int b = sc.nextInt();
- System.out.print("Enter c number here : ");
-int c = sc.nextInt();
-if(a>=b && a>=c){
-  System.out.println(a);
-}
-else if (b>=a && b>=c) {
-  System.out.println(b);
-}
-else{
-  System.out.println(c);
-}
-sc.close();
+     System.out.println(2*1);
+ System.out.println(2*2);
+ System.out.println(2*3);
+  System.out.println(2*4);
+   System.out.println(2*5);
+    System.out.println(2*6);
+     System.out.println(2*7);
+      System.out.println(2*8);
+       System.out.println(2*9);
+        System.out.println(2*10);
+
   }
 }
