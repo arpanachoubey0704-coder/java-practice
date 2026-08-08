a3ce1ae2193002358eb22241dc730382f2763cb0
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-08 19:37:19 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Loops {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-   int table =  2;
-   int n = sc.nextInt();
-   int m = table*n;
-   System.out.println( "multiplication is " + "2" + "*" + n + "=" + m);
+int n = 2;
+for(int i=1 ; i <=10; i++){
+  System.out.println(n +i+ n*i);
+}
 
   }
 }
