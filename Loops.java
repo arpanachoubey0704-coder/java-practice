c91520e59b4d00c997aef2de4ae9fbdca13a3381
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-09 19:23:51 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
-public class Loops {
+public class Loops{
   public static void main(String[] args) {
-int n = 2;
-for(int i=1 ; i <=10; i++){
-  System.out.println(n + n*i);
-}
-
+    for (int i = 1; i<=100; i++){
+    System.out.println(i);
+    }
   }
 }
