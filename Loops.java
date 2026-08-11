09f89d6f5bf9a966391503678841c3443a5cfa4c
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-11 12:59:23 +0530

    Initial commit

---
+++ Loops.java
@@ -0,0 +1,7 @@
+public class Loops {
+
+  public static void main(String[] args) {
+    int num []= new int[20];
+    System.out.println(num);
+  }
+}
--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        int num []= {1,2,3};
-    }
-}
