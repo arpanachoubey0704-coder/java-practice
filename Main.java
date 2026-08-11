24c00638b72ca69ea22ee5794c01033f311bd70c
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-11 12:57:31 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
-public class Main{
-    public void result(){
-        int  a = 80;
-        System.out.print(a);
-    }
-    public static void main(String[] args) {
-        Main w = new Main();
-    w.result();
-    }
+public class Main {
 
+    public static void main(String[] args) {
+        int num []= new int[10];
+    }
 }
