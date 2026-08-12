4463c4d4b9630b3eae6121108a9da15129baead0
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-12 12:42:43 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,7 +1,10 @@
 public class Loops {
   public static void main(String[] args) {
-    for (int n = 2; n<=10;n+=2){
+    for (int n = 1; n<=10;n++){
+      if(n/2==0)
+      {
       System.out.println(n);
+      }
     }
   }
 }
