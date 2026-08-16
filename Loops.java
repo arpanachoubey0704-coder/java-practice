718ab1d32a42d7bc69e1f956d9882b7330c801de
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-16 20:22:31 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
-public class Loops{
-  public static void main(String[] args) {
-    System.out.println("Hellow");
-  }
+public class Loops {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("java ");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+            }
+            //System.out.println();
+
+    }
 }
