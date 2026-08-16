9d59ff7a1249488897b75b27ba6149a2356176d6
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-16 20:19:48 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,16 +1,6 @@
-public class Loops {
+
+public class Loops{
   public static void main(String[] args) {
-    System.out.print(" Even number : ");
-    for (int n=2; n <=10; n+=2){
-      System.out.print( n + " ");
-    }
-    System.out.println();
-     System.out.print(" ODD number : ");
-    for (int n=1; n <=10; n++){
-      System.out.println("numbers are : ");
-      if(n%4 !=1){
-      System.out.print( n + " ");
-      }
-    }
+    System.out.println("Hellow");
   }
 }
