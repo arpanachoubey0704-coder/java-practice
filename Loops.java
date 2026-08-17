1884f4e2acc32526e7c827ade809f08d81e277c6
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-17 12:05:16 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,16 +1,9 @@
 import java.util.Scanner;
 
 public class Loops {
-
-    public static void main(String[] args) {
-            Scanner sc=new Scanner(System.in);
-            System.out.println("java ");
-            for(int i=0;i<3;i++){
-                String s1=sc.next();
-                int x=sc.nextInt();
-                //Complete this line
-            }
-            //System.out.println();
-
-    }
+  public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println(a);
+  }
 }
