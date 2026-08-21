649e9d9b9fb10b2cc9db83b43778854d1264ba95
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-21 23:33:03 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main{
   public static void main(String[] args) {
-    System.out.println(" HEllow world ");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println(" HEllow world " +a);
+
+    }
+    //System.out.println(" HEllow world ");
   }
-}
