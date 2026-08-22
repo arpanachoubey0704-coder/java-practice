6c43c16bf89d30ef367cae5d2ee8550557d2932c
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-22 20:04:33 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
+    System.out.println( n + " + " +n + " = " + (n + n) );
+    System.out.println(n + " + " +n + " = " + (n - n));
+    System.out.println(n + " + " +n + " = " + (n * n));
+    System.out.println(n + " + " +n + " = " + (n / n));
     sc.close();
   }
 }
