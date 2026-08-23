962fd3ec2972358c77dcd90050e430f685a810b9
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-23 20:49:25 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
-import java.util.Scanner;
-
 public class Main{
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    System.out.println( n + " + " +n + " = " + (n + n) );
-    System.out.println(n + " - " +n + " = " + (n - n));
-    System.out.println(n + " * " +n + " = " + (n * n));
-    System.out.println(n + " / " +n + " = " + (n / n));
-    sc.close();
+    int a = 9;
+    System.out.println(a);
   }
 }
