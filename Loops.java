e3680202f0c0fce42785cb6009bff20651e77f47
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-25 23:48:49 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Loops{
   public static void main(String[] args) {
     Scanner sc= new Scanner (System.in);
-    int n = sc.nextInt();
-    System.out.println(n/2);
+    boolean n = sc.nextBoolean();
+    System.out.println(n);
     sc.close();
   }
 }
