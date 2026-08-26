3dbc47ec6cce51b53356a3eab879074093e98f82
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-26 20:37:38 +0530

    Initial commit

--- Operator.java
+++ Operator.java
@@ -1,29 +1,5 @@
-import java.util.Scanner;
-
-public class Operator {
-
+public class Operator{
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter the year :  ");
-int Year = sc.nextInt();
-if (Year % 4 == 0 ){
-  if (Year % 100==0){
-    if (Year % 400==0){
-System.out.println(Year+ " is leap year ");
-    }
-    
-    else {
-      System.out.println(" this is the normal year");
-    }
-
-  }
-  else {
-    System.out.println("this is the leap year");
-  }
-}
-else {
-  System.out.println(Year+ " not a  leap year");
-}
-sc.close();
+    System.out.println("HEllow");
   }
 }
