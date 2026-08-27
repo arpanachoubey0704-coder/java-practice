878c76134be014b672705d203afa59486dd4cf09
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-27 22:45:21 +0530

    Initial commit

--- variableanddatatype.java
+++ variableanddatatype.java
@@ -1,20 +1,8 @@
-// Your name
-// Your age
-// Your CGPA
-// Your branch initial
-// Whether you are a student
 public class variableanddatatype {
 
   public static void main(String[] args) {
-    String name = "Arpana Choubey";
-    int age = 21;
-    float CGPA = 7.1f;
-    char branch = 'i';
-    boolean isstudent = true;
-System.out.println(name);
-System.out.println(age);
-System.out.println(CGPA);
-System.out.println(branch);
-System.out.println(isstudent);
+    int a = 10;
+    double b = 5.5;
+    System.out.println(a+b);
   }
 }
