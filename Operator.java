55b10546a421d5bc9c046272c5e4c548ff104f01
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-27 22:19:48 +0530

    Initial commit

--- Operator.java
+++ Operator.java
@@ -1,5 +1,9 @@
-public class Operator{
+//import java.util.Scanner;
+
+public class Operator {
   public static void main(String[] args) {
-    System.out.println("HEllow");
+    byte age = 21;
+    System.out.println(age);
+
   }
 }
