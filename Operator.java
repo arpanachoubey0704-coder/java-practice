10182312c0fc0bd0264841a19803666df85f2476
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-29 22:56:09 +0530

    Initial commit

--- Operator.java
+++ Operator.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class Operator{
   public static void main(String[] args) {
-System.out.print(" Eneter your age : ");
+System.out.print(" Eneter your name : ");
     Scanner sc = new Scanner (System.in);
+String name = sc.nextLine();
+System.out.println(name);
+System.out.print(" Eneter your age : ");
+    //Scanner sc = new Scanner (System.in);
   int a = sc.nextInt();
   System.out.println(a);
 System.out.print(" Eneter your grade : ");
