00c1a38667a1fb23e1fcf682429914eabe2e3d44
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-08-29 22:50:44 +0530

    Initial commit

--- Operator.java
+++ Operator.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class Operator{
   public static void main(String[] args) {
+System.out.print(" Eneter your age : ");
     Scanner sc = new Scanner (System.in);
   int a = sc.nextInt();
   System.out.println(a);
+System.out.print(" Eneter your grade : ");
+  char c = sc.next().charAt(0);
+System.out.println(c);
   sc.close();
   }
 }
