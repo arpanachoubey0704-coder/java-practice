bf5631f485c400b4adccc42bdf18d5bd05fd266b
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-01 22:10:29 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    if(a%2==0){
-System.out.println("number is even");
+    System.out.print(" Enter Student marks : ");
+    int marks = sc.nextInt();
+    if(marks>=40){
+      System.out.print("Pass");
     }
     else{
-      System.out.println("number is odd");
+      System.out.println("Fail");
     }
     sc.close();
   }
