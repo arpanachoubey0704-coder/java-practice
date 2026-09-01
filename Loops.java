3017efab61ece9d5227309a080895df38a9823e6
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-01 17:15:19 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-
 public class Loops{
   public static void main(String[] args) {
-    Scanner sc= new Scanner (System.in);
-    boolean n = sc.nextBoolean();
-    System.out.println(n);
-    sc.close();
+    System.out.println("Hellow World");
   }
 }
