72065b161c4b555ead11aeef487b31af7ed7715c
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-01 21:02:55 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
-    if(a>=0){
+    if(a<=0){
 System.out.println("number is Zero");
     }
     else{
