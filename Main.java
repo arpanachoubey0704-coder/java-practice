4082659f98f7f8e75e11970a6408632949a87302
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-01 21:10:40 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ public class Main{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
-    if(a<=0){
-System.out.println("number is Zero");
+    if(a%2==0){
+System.out.println("number is even");
     }
     else{
-      System.out.println("number is not zero");
+      System.out.println("number is odd");
     }
     sc.close();
   }
