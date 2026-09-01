04a73c6e3140452a7c98bb6188ae9e2339ca2755
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-01 17:19:43 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -4,7 +4,7 @@ public class Loops{
   public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
     int n = sc.nextInt();
-    System.out.println("Hellow World" +n);
+    System.out.println(" Hellow World : " +n);
     sc.close();
   }
 }
