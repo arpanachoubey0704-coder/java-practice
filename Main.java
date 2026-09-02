88fa457a7ffffeeece723d7fef864dd53f4d0fec
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-02 08:41:20 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    if(n/5<=5){
+    if(n%5==0){
 System.out.println("number is Divisibilty by 5");
     }
     else{
