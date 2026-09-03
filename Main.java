3bb298ba3e784976d36fb2c2bb7d3b0470a5653f
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-03 20:51:20 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,17 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
-
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter any letter here : ");
-    char letter = sc.next().charAt(0);
-    if(letter=='a'||letter== 'e'|| letter=='i' || letter =='o' || letter =='u'){
-System.out.println("Vowel");
+    for(int i = 1; i<=10; i++){
+      System.out.println(i);
     }
-    else{
-      System.out.println("Consonent");
-    }
-    sc.close();
   }
 }
