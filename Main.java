10c6cf2a0e7f7d629807882a356709d60ece4bf6
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-04 08:06:37 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    for(int i = 10; i >=1; i--){
+    for(int i = 1; i <=20; i++){
+      if(i%2==0){
       System.out.println(i);
+      }
     }
   }
 }
