c865fecd6f0c1e3ec130a7140e12b639b3440c24
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-05 10:40:59 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    int n = 5;
-    for(int i = 1; i <=10; i++){
-    
-      System.out.println(i*n);
-      
-    }
+     int n = 90;
+     System.out.println(" Hellow  : " +n);
   }
 }
