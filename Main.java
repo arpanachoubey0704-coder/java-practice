1b0dbe30d667f2b9f09e0a4849f0430aaec940dd
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-05 11:04:38 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
-
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
      int n = sc.nextInt();
-     System.out.println(" Hellow  : " +n);
+     char a = sc.next().charAt(0);
+     boolean t = sc.nextBoolean();
+     double d = sc.nextDouble();
+
+     System.out.println(" Hellow  : " +n + " " +a + " " +t+ " "+d);
      sc.close();
   }
 }
