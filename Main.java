e3c4bac81fe110499c8231d3ca29e8a13f791038
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-07 18:49:59 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-     int n = sc.nextInt();
-     char a = sc.next().charAt(0);
-     boolean t = sc.nextBoolean();
-     double d = sc.nextDouble();
-     System.out.println(" Hellow  your output is here : " +n + " " +a + " " +t);
-     sc.close();
+    Scanner sc = new Scanner (System.in);
+    System.out.print(" Enter number here : " );
+    int n = sc.nextInt();
+    if(n%2==0){
+    System.out.println("Even");
+    }
+    else{
+System.out.println("Odd");
+    }
+    sc.close();
   }
 }
