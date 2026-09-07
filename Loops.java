4a987621ddb4ace80bc865ed8d549dc0344db31f
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-07 18:28:29 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Loops{
-  public static void main(String[] args) {
+      public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
-    System.out.print(" Enter numbere here : ")
-    int n = sc.nextInt();
+    System.out.print(" Enter numbere here : ");
+int n = sc.nextInt();
     if (n<=0){
       System.out.println("Numbere is negative");
     }
     else{
-      System.out.println("Number is negative");
+      System.out.println("Number is positive");
     }
     sc.close();
   }
