0c9ba39aad93995393b7d223c7db59c68da86e67
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-11 11:26:48 +0530

    Initial commit

---
+++ function.java
@@ -0,0 +1,15 @@
+public class function {
+
+  public static void main(String[] args) {
+  //Scanner sc = new Sacnner (System.in);
+  int n = 3;
+  int m = 4;
+  if (n<m){
+    System.out.println("greater");
+  }
+  else{
+    System.out.println("less");
+    //sc.close();
+  }
+  }
+}
