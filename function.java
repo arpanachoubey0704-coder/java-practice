1d55fa132b631e79bbf2d88a6fc3d8bb2e609d73
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-12 20:17:31 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,7 +1,7 @@
 public class function {
   public static void main(String[] args) {
-    int n = 2;
-int m  = 9;
-System.out.println( n+m );
+   String n = "hiiii arpana ", m  = "Welcome";
+
+System.out.println( n + m );
   }
 }
