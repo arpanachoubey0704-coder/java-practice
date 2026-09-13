8045b5add1852045e8a4ff45c705abcd5dbefe57
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-13 20:32:25 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println(" Enter numbere here : ");
-    int a=sc.nextInt();
-    int b=sc.nextInt();
-    int c = sc.nextInt();
-    if(a<=b && a<=c){
-System.out.println(a + " is smallest number ");
-    }
-    else if(b<=c){
-      System.out.println(b + " is smallest number ");
-    }
-    else{
-      System.out.println(c + " is smallest number 11");
-    }
+    System.out.print(" Enter numbere here : ");
+    int n = sc.nextInt();
+    System.out.print(n%10);
     sc.close();
   }
 }
