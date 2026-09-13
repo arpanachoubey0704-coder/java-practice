9e1a6257daec39c369b72704f6c27db574482f8d
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-13 09:41:25 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,7 +1,30 @@
-public class function {
-  public static void main(String[] args) {
-   String n = "hiiii arpana ", m  = "Welcome";
+// Problem 1 — Basic Calculator
 
-System.out.println( n + m );
+// Take two integers from the user and print:
+
+// Addition
+// Subtraction
+// Multiplication
+// Division
+// Remainder
+// public class function {
+  
+// }
+
+import java.util.Scanner;
+
+public class function {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print(" Enter no. here : ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+sc.close();
   }
 }
