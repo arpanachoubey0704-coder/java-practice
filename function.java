c49273cb3314b125cd91afed45b715ec986ad018
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-13 09:55:56 +0530

    Initial commit

--- function.java
+++ function.java
@@ -1,30 +1,16 @@
-// Problem 1 — Basic Calculator
-
-// Take two integers from the user and print:
-
-// Addition
-// Subtraction
-// Multiplication
-// Division
-// Remainder
-// public class function {
-  
-// }
-
 import java.util.Scanner;
 
 public class function {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter no. here : ");
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-System.out.println(a+b);
-System.out.println(a-b);
-System.out.println(a*b);
-System.out.println(a/b);
-System.out.println(a%b);
+  System.out.print(" Enter length here : ");
+  double length = sc.nextDouble();
+  System.out.print(" Enter breadth here : ");
+  double breadth = sc.nextDouble();
+  System.out.println("Area is  : " + length * breadth);
+  System.out.println( " Perimeter is : " + 2*(length+breadth));
 sc.close();
   }
 }
+ 
