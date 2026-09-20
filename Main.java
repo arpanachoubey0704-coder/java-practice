388776e77f5f44baf9f39b97c9e5f45c8e17bb3e
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-20 22:23:19 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter numbere here : ");
-    int n = sc.nextInt();
-    System.out.print(n%10);
-    sc.close();
+  public static void main (String [] args ){
+  int n = 1 ; 
+  System.out.println(1+n);
   }
 }
