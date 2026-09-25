0066fdcb2b5906740c8a76c5b072bc5882042613
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-26 00:22:09 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter numbere here : ");
+    System.out.print(" Enter tempreture here : ");
+int tempreture = sc.nextInt();
+  
+    if(tempreture<25){
+ System.out.println("below freezing");
+    }
+else if (tempreture==25) {
+  System.out.println("norma");
+}
 
-    int n = sc.nextInt();
-    if(n<0){
-      System.out.println( "Invaild number");
-    }
-   else if (n>=18){
-      System.out.println("eligible to vote");
-    }
     else{
-      System.out.println("Not eligible to vote");
+      System.out.println("above normal");
     }
     sc.close();
   }
