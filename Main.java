0d76281e99cc2819c4033d5084b7ce2baf3442f8
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-25 23:42:08 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
+
 public class Main {
-  
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(" Enter numbere here : ");
+
+    int n = sc.nextInt();
+    if (n>0){
+      System.out.println("NO. is Poitive");
+    }
+    else if (n==0) {
+      System.out.println("No. is Zero");
+    }
+    else{
+      System.out.println("No. is Negative");
+    }
+    sc.close();
+  }
 }
