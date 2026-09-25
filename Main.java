51b8d40567458d339a25cffd39fd489e50819335
Arpana Choubey <arpanachoubey0704@gmail.com>
2026-09-25 23:49:41 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
-
-
 public class Main {
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println(" Enter numbere here : ");
+    System.out.print(" Enter numbere here : ");
 
     int n = sc.nextInt();
-    if (n>0){
-      System.out.println("NO. is Poitive");
-    }
-    else if (n==0) {
-      System.out.println("No. is Zero");
+    if (n>=18){
+      System.out.println("eligible to vote");
     }
     else{
-      System.out.println("No. is Negative");
+      System.out.println("Not eligible to vote");
     }
     sc.close();
   }
